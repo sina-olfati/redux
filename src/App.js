396498a63c9d62bf688811db9021a1f5ec684047
sina <sinaolfati5@gmail.com>
2022-06-21 03:05:38 -0700

    reducer 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import './App.css';
-import React, { useState, createContext } from 'react';
-import Context from './components/Context';
-
-
-export const Number = createContext()
-export const Name = createContext()
+import React from 'react';
+import Reducer from './components/Reducer';
 
 
 function App() {
-  const [count, setCount] = useState(0);
-  const name = "Sinaaaaaa"
 
   return (
     <div className="App">
-      <Number.Provider value={count}>
-      <Name.Provider value={name}>
-      Sina
-      <br />
-      <button onClick={() => {setCount(count + 1)}}>+</button>
-      <Context />
-      </Name.Provider>
-      </Number.Provider>
+       <Reducer />
     </div>
   );
 }
